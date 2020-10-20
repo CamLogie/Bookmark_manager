6704d41b8ce576d46f26a76ce56a66c7d14d3772
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-20 16:18:35 +0100

    Updated unit test to stub out #bookmarks method

--- spec/unit/bookmark_spec.rb
+++ spec/unit/bookmark_spec.rb
@@ -2,6 +2,7 @@ require "./lib/bookmark.rb"
 
 describe Bookmark do
   it "responds to the class method .all by return a list of bookmarks" do
-    expect(Bookmark.bookmarks).to eq Bookmark.bookmarks
+    allow(Bookmark).to receive(:bookmarks).and_return('https://www.postgresqltutorial.com/postgresql-update/')
+    expect(Bookmark.bookmarks).to eq('https://www.postgresqltutorial.com/postgresql-update/')
   end
 end
